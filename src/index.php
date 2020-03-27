7579a5ab976af2a2d0f02c793941bf16029a4f07
AnwaarAhmedshah <fa15be0011@maju.edu.pk>
2020-03-27 19:21:31 +0500

    This is Dockerize App for Assignment

--- src/index.php
+++ src/index.php
@@ -7,12 +7,13 @@ if ($conn->connect_error) {
 
 $sql = "SELECT name FROM users";
 $result = $conn->query($sql);
-
+echo "<h1 style='color:red;'> My MAJU CLOUD SHOP </h1> <h1 style='color:red;'> SYED ANWAR AHMED SHAH </h1><b><ol>";
 if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
-        echo $row['name']."<br>";
+        echo "<li> $row[name] </li> <br>";
     }
+    echo "</ol>";
 } else {
     echo "0 results";
 }
